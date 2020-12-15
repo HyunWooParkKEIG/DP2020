57d25d03c73b1a13c072709d7edfe7a0db3023f8
HyeonWooPark <yey6687@daum.net>
2020-12-15 12:34:46 +0900

    changed HTML code in HTMLExporter class

--- src/com/holub/database/HTMLExporter.java
+++ src/com/holub/database/HTMLExporter.java
@@ -16,42 +16,44 @@ public class HTMLExporter implements Table.Exporter
                                int height,
                                Iterator columnNames ) throws IOException
     {	this.width = width;
+        out.write("<!DOCTYPE html>\n");
+        out.write("<html>\n");
         out.write("\t<head>\n");
         out.write("\t\t<title>");
-        out.write(tableName == null ? "anonymous" : tableName );
-        out.write("</title>\n");
+        out.write(tableName == null ? "anonymous" : tableName + "</title>\n");
         out.write("\t</head>\n");
         out.write("\t<body>\n");
-        out.write("\t\t<h2>\n");
-        out.write(tableName == null ? "anonymous" : tableName + "</h2>");
+        out.write("\t\t<h2>");
+        out.write(tableName == null ? "anonymous" : tableName + "</h2>\n");
         out.write("\t\t<table>\n");
         out.write("\t\t\t<tbody>\n");
-        storeRow( columnNames ); // comma separated list of columns ids
+
+        storeRow( columnNames );
+
+
     }
 
     public void storeRow( Iterator data ) throws IOException
     {	int i = width;
         if(data.hasNext()){
-            out.write("\t\t\t\t<tr>");
+            out.write("\t\t\t\t<tr>\n");
         }
-        while( data.hasNext() )
-        {	Object datum = data.next();
+        while( data.hasNext() ) {
+            Object datum = data.next();
             if( datum != null ){
-                out.write("<td>");
+                out.write("\t\t\t\t\t<td>");
                 out.write( datum.toString() );
             }
 
             if( --i > 0 ){
-                out.write("</td>");
+                out.write("</td>\n");
             }
         }
-        out.write("</tr>\n");
+        out.write("</td>\n");
+        out.write("\t\t\t\t</tr>\n");
     }
 
-    public void startTable() throws IOException {
-        out.write("<!DOCTYPE html>\n");
-        out.write("<html>\n");
-    }
+    public void startTable() throws IOException {/*nothing to do*/}
     public void endTable()   throws IOException {
         out.write("\t\t\t</tbody>\n");
         out.write("\t\t</table>\n");
